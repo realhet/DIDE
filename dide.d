51238eac3acc5716d6316b4c4c15cf924ad9915a
realhet <real_het@hotmail.com>
2020-12-23 20:41:37 +0100

    appendCode

--- dide.d
+++ dide.d
@@ -2,8 +2,8 @@
 //@import c:\d\libs
 //@ldc
 //@compile -m64 -mcpu=athlon64-sse3 -mattr=+ssse3
-//@release
-///@debug
+///@release
+//@debug
 
 ///@run $ c:\d\libs\het\test\syntaxTestText.d
 //@run $ dide.d
@@ -16,7 +16,10 @@
 import het, het.ui, het.tokenizer, het.keywords;
 
 /*comment1*/@safe void testFunction()(/*comment3*/) /*comment4*/ { //comment2
-  if(a) b;
+  if(a) b!(c) = !(d);
+
+  struct S { int a; };
+  S s = { a:5 };
 }
 
 public{// Utility stuff ///////////////////////////////////////////////////////////////////
@@ -42,10 +45,47 @@ bool isSimpleToken(in Token t){
     //these need a sub-container
     case comment, literalString, literalChar                   : return false;
     //there are a few block opening operators, but the rest are simple
-    case operator: return !t.id.among(oproundBracketOpen, opsquareBracketOpen, opcurlyBracketOpen, optokenString);
+    case operator: return !t.id.among(oproundBracketOpen, opsquareBracketOpen, opcurlyBracketOpen, optokenString); // !( is handled later
     //unknown is forbidden
     case unknown: assert(0, "Fatal error: unknown token not allowed here");
   }
+}
+
+
+/+bool previousToken_skipComments(in Token[] tokens, in sizediff_t idxIn, out sizediff_t idxOut){
+  auto i = idxIn-1;
+  while(i>=0){
+    if(tokens[i].isComment){
+      i--;
+    }else{
+      idxOut = i;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool backTrackTemplateRoundBracketOpen(in Token[] tokens, ref sizediff_t idx){
+  if(tokens[idx].isOperator(oproundBracketOpen)){
+    auto idx2 = idx;
+    if(previousToken_skipComments(tokens, idx, idx2) && tokens[idx2].isOperator(opnot)) {
+      idx = idx2;
+      return true;
+    }
+  }
+  return false;
+}+/
+
+Token[] fetchSimpleTokens(ref Token[] tokens){
+  auto idx = tokens.countUntil!(not!isSimpleToken);
+
+  if(idx<0) idx = tokens.length; //take all if -1
+  /+else backTrackTemplateRoundBracketOpen(tokens, idx); //handle template !(
+    The template parameter token sequence is identifier!( not just !(. It's not reasonable to put it into a container, just handle the ( ), and put the ! in front manually. +/
+
+  auto res = tokens[0..idx];
+  tokens = tokens[idx..$];
+  return res;
 }
 
 void appendCode(Row row, Token[] tokens, SourceCode sourceCode, bool setBkColor=true){
@@ -53,14 +93,91 @@ void appendCode(Row row, Token[] tokens, SourceCode sourceCode, bool setBkColor=
 
   auto ts = tsNormal;  ts.applySyntax(0);
 
-  void appendSimpleCode(R)(R tokens){
+  void appendSimpleCode(in Token[] tokens){
     if(tokens.length){
       auto st = tokens[0].pos, en = tokens[$-1].endPos;
       het.uibase.appendCode(row, sourceCode.text[st..en], sourceCode.syntax[st..en], s => ts.applySyntax(s), ts);
     }
   }
+  if(tokens.empty) return;
 
-  foreach(isSimple, len; tokens.map!isSimpleToken.group){
+  size_t simpleStartPos = tokens[0].pos,
+         simpleEndPos;
+
+  while(tokens.length){
+    auto simpleTokens = tokens.fetchSimpleTokens;
+
+    /*assert*/enforce(simpleTokens.length || tokens.length, "At this point it either must have simpleTokens or complicated tokens.");
+
+    simpleEndPos = tokens.length ? tokens.front.pos //if there is a complicated token after this, thake the first complicated token's startpos
+                                 : simpleTokens.back.endPos; //last endPos of the simpleTokens.
+
+    //emit simpleTokens as text if there's any
+    if(simpleStartPos < simpleEndPos) het.uibase.appendCode(row, sourceCode.text  [simpleStartPos..simpleEndPos],
+                                                                 sourceCode.syntax[simpleStartPos..simpleEndPos], s => ts.applySyntax(s), ts);
+
+    if(tokens.length){ //fetch complicated token(s)
+
+      auto searchUntil(int op){
+        auto level = tokens[0].level;
+
+        auto res = tokens.countUntil!(t => t.isOperator(op) && t.level==level);
+        /*assert*/enforce(res>0, "Fatal error: inconsitstent brackets.");
+        return res+1; //include the end bracket too
+      }
+
+      //localize the complicated token range
+      size_t len, bracketStartIdx/*for template !(*/;
+      bool isTemplate;
+      with(TokenKind) switch(tokens[0].kind){
+        case comment, literalString, literalChar : len = 1; break;
+        case operator: {
+          switch(tokens[0].id){
+            case oproundBracketOpen:  len = searchUntil(oproundBracketClose ); break;
+            case opsquareBracketOpen: len = searchUntil(opsquareBracketClose); break;
+            case opcurlyBracketOpen:  len = searchUntil(opcurlyBracketClose ); break;
+            default: break;
+          }
+        }
+        default: break;
+      }
+
+      /*assert*/enforce(len>0, "Unable to fetch complicated token array");
+
+      //actual fetch
+      auto complicatedTokens = tokens[0..len];
+      tokens = tokens[len..$];
+
+      //save the start of the next simple tokens including whitespace
+      simpleStartPos = complicatedTokens.back.endPos;
+
+      //append the complicated object
+
+      auto obj = new Row;
+      obj.margin = "2";
+      obj.padding = "2";
+      obj.border = "1";
+      obj.border.color = clFuchsia;
+
+      //het.uibase.appendCode(obj, sourceCode.text[simpleStartPos..simpleEndPos], sourceCode.syntax[simpleStartPos..simpleEndPos], s => ts.applySyntax(s), ts);
+
+      if(len==1){
+        if(complicatedTokens[0].isComment){
+
+        }else{ //must be a literal string or char
+
+        }
+      }else{
+        //recursion
+        appendCode(obj, complicatedTokens[bracketStartIdx+1..$-1], sourceCode);
+      }
+
+      row.append(obj);
+
+    }
+  }
+
+/*  foreach(isSimple, len; tokens.map!isSimpleToken.group){
     //todo if not simple, there must be a special thing that eats up more than one tokens, so .group is not good here
 
     auto act = tokens.takeExactly(len);
@@ -73,7 +190,7 @@ void appendCode(Row row, Token[] tokens, SourceCode sourceCode, bool setBkColor=
     }
 
     tokens.popFrontExactly(len);
-  }
+  }*/
 }
 
 }public{// UI - Code integration ///////////////////////////////////////////////////
