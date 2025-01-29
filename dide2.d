0b05c17198d4651d87a859d13afea72d4d106f68
realhet <real_het@hotmail.com>
2025-01-29 17:09:26 +0100

    a

--- dide2.d
+++ dide2.d
@@ -881,6 +881,7 @@ version(/+$DIDE_REGION main+/all)
 														ChkBox(showHitTest, "HitTest"); 
 														ChkBox(showUndoStack, "Undo stack"); 
 														ChkBox(showResyntaxQueue, "Resyntax Queue"); 
+														ChkBox(showFPS	, "Show FPS Graph"); 
 													}
 												); 
 											}	break; 
@@ -4922,14 +4923,15 @@ class Workspace : Container, WorkspaceInterface
 													selected(lookInAllModules)
 												)
 											) lookInAllModules.toggle; 
-											if(
-												Btn(
-													"…", hint("Advanced search options"),
-													selected(advancedSearchOptionsVisible)
-												)
-											) advancedSearchOptionsVisible.toggle; 
 										}
 									); 
+									
+									if(
+										Btn(
+											"⚙", hint("Advanced search options"),
+											selected(advancedSearchOptionsVisible)
+										)
+									) advancedSearchOptionsVisible.toggle; 
 									
 									if(
 										Btn(
@@ -5065,123 +5067,7 @@ class Workspace : Container, WorkspaceInterface
 			{ if(visible.chkClear) { searchText = ""; }} 
 			
 			DirNode* focusedNode; 
-			
-			void UI_outlinePanel(Workspace workspace, View2D view, bool justActivated)
-			{
-				with(im)
-				{
-					Column(
-						{
-							//Keyboard shortcuts
-							auto 	kcOutlineZoom	= KeyCombo("Enter"), //only when edit is focused
-								kcOutlineClose	= KeyCombo("Esc"); //always
-							
-							void sw() { outerWidth = fh*22; } 
-							
-							Row(
-								{
-									sw; Text("Outline"); .Container editContainer; 
-									
-									const searcHash = searchText.hashOf; 
-									static size_t lastSearchHash; //Todo: static is ugly. It's a workspace property
-									const searchHashChanged = lastSearchHash.chkSet(searcHash); 
-									
-									
-									if(
-										Edit(searchText, ((justActivated).genericArg!q{focusEnter}), { flex = 1; editContainer = actContainer; })
-										|| justActivated || searchHashChanged
-									)
-									{ NOTIMPL; }
-									
-									BtnRow({ if(Btn("⚙", hint("Setup"), selected(setupVisible))) setupVisible.toggle; }); 
-									
-									if(
-										Btn(
-											bold(symbol("ChevronRight")), { innerWidth = fh; }, 
-											kcOutlineClose, hint("Close panel.")
-										)
-									)
-									{ deactivate; }
-								}
-							); 
-							
-							updateTreeView; 
-							
-							const treeIsEmpty = treeView.root.subNodes.empty; 
-							
-							if(treeIsEmpty) setupVisible = true; 
-							
-							if(setupVisible)
-							{
-								Row(
-									{
-										sw; Text("Exts: "); 
-										Edit(extensions, { flex = 1; }); 
-									}
-								); 
-								Row(
-									{
-										sw; Text("Paths: "); 
-										if(Btn("Add", selected(treeIsEmpty ? blink>.5f : false)))
-										{
-											static Path lastPath; 
-											auto p = browseForFolder(frmMain.hwnd, "Add path to Outline.", lastPath); 
-											if(p) { lastPath = p; addRootPath(p); }
-										}
-										
-										const canRemove = focusedNode && focusedNode.isPath && indexOfRootPath(focusedNode.asPath)>=0; 
-										if(Btn("Remove", enable(canRemove)))
-										{
-											removeRootPath(focusedNode.asPath); 
-											focusedNode = null; 
-										}
-									}
-								); 
-							}
-							
-							treeView.UI(
-								{
-									sw; outerHeight = frmMain.viewGUI.screenBounds_anim.height-128; 
-									/+Todo: fucking lame. Fix aligning engine.+/
-									/+Bug: When lots of items in the tree, moving the window by mouse become fucking slow. <1FPS 🤬+/
-								},
-								((DirNode* n){
-									if(
-										WhiteBtn(
-											{
-												n.UI; border.width = 0; 
-												padding = "0 4 0 0"; margin = "0"; 
-												if(auto img = (cast(.Img)(actContainer.subCells.frontOrNull)))
-												{ img.flags.clickable = false; }
-											}, 
-											((n.identityStr).genericArg!q{id}), selected(focusedNode==n)
-										)
-									)
-									{ focusedNode=n; }
-								})
-							); 
-						}
-					); 
-				}
-				
-			} 
-			
-			bool UI(Workspace workspace, View2D view)
-			{
-				with(im)
-				{
-					{
-						bool justActivated; 
-						if(activateRequest.chkClear)
-						{ visible = justActivated = true; }
-						
-						if(visible)
-						{ UI_outlinePanel(workspace, view, justActivated); }
-						
-						return visible; 
-					}
-				}
-			} 
+			
 			static struct DirNode
 			{
 				import std.sumtype; 
@@ -5263,18 +5149,187 @@ class Workspace : Container, WorkspaceInterface
 					else	open; 
 				} 
 				
-				void UI()
+				void UI(void delegate() fun=null)
 				{
 					with(im)
 					{
 						void Img(string s) { Spacer(4); im.Img(`icon:\`~s~`&small`); Spacer(4); } 
 						_node.match!
 						(
-							((in File  f){ Img('.'~f.ext); Text(f.name); }), 
-							((in Path p){ Img(((p.fullPath.isWild("?:")) ?(p.fullPath):(`folder`))~'\\'); Text(bold(p.name)); })
+							((in File  f){ Img('.'~f.ext); if(fun) fun(); Text(f.name); }), 
+							((in Path p){ Img(((p.fullPath.isWild("?:")) ?(p.fullPath):(`folder`))~'\\'); if(fun) fun(); Text(bold(p.name)); })
 						); 
 					}
 				} 
+			} 
+			void UI_outlinePanel(Workspace workspace, View2D view, bool justActivated)
+			{
+				with(im)
+				{
+					Column(
+						{
+							//Keyboard shortcuts
+							auto 	kcOutlineZoom	= KeyCombo("Enter"), //only when edit is focused
+								kcOutlineClose	= KeyCombo("Esc"); //always
+							
+							void sw() { outerWidth = fh*22; } 
+							
+							Row(
+								{
+									sw; Text("Outline"); .Container editContainer; 
+									
+									const searcHash = searchText.hashOf; 
+									static size_t lastSearchHash; //Todo: static is ugly. It's a workspace property
+									const searchHashChanged = lastSearchHash.chkSet(searcHash); 
+									
+									
+									if(
+										Edit(searchText, ((justActivated).genericArg!q{focusEnter}), { flex = 1; editContainer = actContainer; })
+										|| justActivated || searchHashChanged
+									)
+									{ NOTIMPL; }
+									
+									BtnRow({ if(Btn("⚙", hint("Setup"), selected(setupVisible))) setupVisible.toggle; }); 
+									
+									if(
+										Btn(
+											bold(symbol("ChevronRight")), { innerWidth = fh; }, 
+											kcOutlineClose, hint("Close panel.")
+										)
+									)
+									{ deactivate; }
+								}
+							); 
+							
+							updateTreeView; 
+							
+							const treeIsEmpty = treeView.root.subNodes.empty; 
+							
+							if(treeIsEmpty) setupVisible = true; 
+							
+							if(setupVisible)
+							{
+								Row(
+									{
+										sw; Text("Exts: "); 
+										Edit(extensions, { flex = 1; }); 
+									}
+								); 
+								Row(
+									{
+										sw; Text("Paths: "); 
+										if(Btn("Add", selected(treeIsEmpty ? blink>.5f : false)))
+										{
+											static Path lastPath; 
+											auto p = browseForFolder(frmMain.hwnd, "Add path to Outline.", lastPath); 
+											if(p) { lastPath = p; addRootPath(p); }
+										}
+										
+										const canRemove = focusedNode && focusedNode.isPath && indexOfRootPath(focusedNode.asPath)>=0; 
+										if(Btn("Remove", enable(canRemove)))
+										{
+											removeRootPath(focusedNode.asPath); 
+											focusedNode = null; 
+										}
+									}
+								); 
+							}
+							
+							treeView.UI(
+								{
+									sw; outerHeight = frmMain.viewGUI.screenBounds_anim.height-128; 
+									/+Todo: fucking lame. Fix aligning engine.+/
+									/+Bug: When lots of items in the tree, moving the window by mouse become fucking slow. <1FPS 🤬+/
+								},
+								((DirNode* n){
+									const 	isFile = n.isFile, 
+										isPath = n.isPath,
+									fullName = 	isFile 	? n.asFile.fullName : 
+										isPath 	? n.asPath.fullPath : ""; 
+									Module mod; if(isFile && fullName!="")
+									mod = frmMain.workspace.findModule(fullName.File/+Opt: this is a slow query+/); 
+									
+									if(
+										WhiteBtn(
+											{
+												border.width = 0; padding = "0 4 0 0"; margin = "0"; 
+												
+												n.UI(
+													{
+														if(isFile)
+														{
+															float spc = 1; 
+															if(mod)	{ Led(true, ((mod.changed)?(clYellow):(clLime))); spc -= .7; }
+															Text(' '); 
+															actContainer.subCells.back.outerWidth = fh*spc; 
+														}
+													}
+												); 
+												
+												if(auto img = (cast(.Img)(actContainer.subCells.frontOrNull)))
+												{ img.flags.clickable = false; }
+											}, 
+											((n.identityStr).genericArg!q{id}), selected(focusedNode==n)
+										)
+									)
+									{
+										focusedNode=n; 
+										auto ws = frmMain.workspace; /+Todo: bad dependency: frmMain+/
+										
+										void jumpTo(string prefix, bool doZoom=false)
+										{
+											const p = prefix.lc; 
+											if(
+												const bnd = 	ws.modules
+													.filter!((m)=>(m.file.fullName.map!toLower.startsWith(p))).cache
+													.map!((m)=>(m.outerBounds)).fold!"a|b"(bounds2.init)
+											)
+											{
+												addInspectorParticle(bnd, clWhite, bounds2(bnd.center, ((1).genericArg!q{radius})), .125f); 
+												
+												with(frmMain.view)
+												{
+													if(doZoom)	{ scrollZoomIn(bnd); }
+													else	{ scrollZoom(bnd); }
+												}
+											}
+										} 
+										
+										version(/+$DIDE_REGION Detect doubleClick+/all)
+										{ static string lastFullName; static DateTime lastClickTime; /+Todo: nasty static+/}
+										
+										
+										if(fullName!="")
+										{
+											const isDoubleClick = lastFullName==fullName && now-lastClickTime < 0.4*second; 
+											lastClickTime = now; 	lastFullName = fullName; 
+											
+											jumpTo(fullName, isDoubleClick); 
+										}
+									}
+								})
+							); 
+						}
+					); 
+				}
+				
+			} 
+			
+			bool UI(Workspace workspace, View2D view)
+			{
+				with(im)
+				{
+					{
+						bool justActivated; 
+						if(activateRequest.chkClear)
+						{ visible = justActivated = true; }
+						
+						if(visible)
+						{ UI_outlinePanel(workspace, view, justActivated); }
+						
+						return visible; 
+					}
+				}
 			} 
 		} 
 	}version(/+$DIDE_REGION+/all)
@@ -7108,7 +7163,8 @@ class Workspace : Container, WorkspaceInterface
 		
 		protected void drawFolders(Drawing dr, RGB clFrame, RGB clText)
 		{
-			//Todo: detect changes and only collect info when changed.
+			//Opt: detect changes and only collect info when changed.
+			auto _間=init間; scope(exit) ((0x356B135B2D627).檢((update間(_間)))); 
 			
 			const paths = modules.map!(m => m.file.path.fullPath).array.sort.uniq.array; 
 			
